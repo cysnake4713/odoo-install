f35ad492a4b2a2515f5331772a1faee201809d4c
cysnake4713 <cysnake4713@gmail.com>
2014-07-04 14:03:54 +0800

    [IMP]format backup strategy

--- lib/backup.sh
+++ lib/backup.sh
@@ -12,5 +12,5 @@ date_before=`date -d "6 month ago" +%Y%m`
 
 rm -f ${BACKUP_HOME}/${date_before}*
 
-sudo su - postgres -c "pg_dump -Fc ${DB_NAME} > ${BACKUP_HOME}/datenow_backup.dump"
+sudo su - postgres -c "pg_dump -Fc ${DB_NAME} > ${BACKUP_HOME}/${datenow}_backup.dump"
 
